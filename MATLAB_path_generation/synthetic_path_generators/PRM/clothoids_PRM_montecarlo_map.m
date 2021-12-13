01635ed23a6559f51ceaf51bf9c42d020e7992c3
Placido <planora@hotmail.com>
2021-12-13 17:18:59 +0100

    clothoids_PRM_montecarlo_map fixed the spline function call

--- MATLAB_path_generation/synthetic_path_generators/PRM/clothoids_PRM_montecarlo_map.m
+++ MATLAB_path_generation/synthetic_path_generators/PRM/clothoids_PRM_montecarlo_map.m
@@ -10,7 +10,7 @@ hold on
 map1 = copy(map);
 inflate(map1,0.2/res);
 
-LVEC = 0.5/res;
+LVEC = 0.8/res;
 
 time = clock();
 rng(time(6));
@@ -18,7 +18,7 @@ rng(time(6));
 FileName = "Prova.txt";
 fid = fopen(FileName, 'w');
 
-for i=1:1
+for i=1:10
     prm = mobileRobotPRM(map1,100);
 
     P1 = [3 + rand()*2, 1 + rand()*9]./res;
@@ -27,9 +27,12 @@ for i=1:1
     a2 = -rand()*pi/2;
 
     samples = [];
+    angles = [];
 
     path = findpath(prm,P1,P2);
         
+%     plot(path(:,1),path(:,2),'LineWidth',2)
+
     path(2,:)=[];
     path(end-1,:)=[];
     path = path(1:ceil(length(path)/6):end,:);
@@ -43,40 +46,42 @@ for i=1:1
 
 %     plot(path(:,1),path(:,2),'ro');
 
+%     angle = a1;
+%     angles = [angles; angle];
+% 
+%     for j=1:length(path)-1
+%         angle0 = angle;
+%         C = ClothoidCurve();
+%         x1=path(j,1);
+%         y1=path(j,2);
+%         x2=path(j+1,1);
+%         y2=path(j+1,2);
+%         angle1 = atan2d((y2 - y1),(x2 - x1))/180*pi;
+%         if j+2 <= length(path)
+%             x3=path(j+2,1);
+%             y3=path(j+2,2);
+%             angle2 = atan2d((y3 - y2),(x3 - x2))/180*pi;
+%             angle = (angle1+angle2)/2;
+%         else
+%             angle = a2;
+%         end
+%         iter = C.build_G1(x1, y1, angle0, x2, y2, angle );
+%         angles = [angles; angle];
+%         C.plot( 1000, '-b', 'LineWidth', 3 );
+% 
+%     end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    CL = ClothoidSplineG2(); 
-    SL = CL.buildP2custom(path(:,1), a1,  path(:,2), a2);
     npts = 1000;
-    SL.plot(npts);
+    CL = ClothoidSplineG2();
+    SL = CL.buildP1(path(:,1), path(:,2),a1,a2);
+    SL.plot(npts,{'Color','blue','LineWidth',2},{'Color','blue','LineWidth',2});
 
-    angle = a1;
+    [a b] = SL.points(100);
+    samples = [samples; a' b'];
 
-    for j=1:length(path)-1
-        angle0 = angle;
-        C = ClothoidCurve();
-        x1=path(j,1);
-        y1=path(j,2);
-        x2=path(j+1,1);
-        y2=path(j+1,2);
-        angle1 = atan2d((y2 - y1),(x2 - x1))/180*pi;
-        if j+2 <= length(path)
-            x3=path(j+2,1);
-            y3=path(j+2,2);
-            angle2 = atan2d((y3 - y2),(x3 - x2))/180*pi;
-            angle = (angle1+angle2)/2;
-        else
-            angle = a2;
-        end
-        iter = C.build_G1(x1, y1, angle0, x2, y2, angle );
-        C.plot( 1000, '-b', 'LineWidth', 2 );
+    fmt = [repmat('%10.5f ', 1, length(samples)-1), '%10.5f\n'];
+    fprintf(fid, fmt, samples);
 
-        [a b] = C.points(100);
-        samples = [samples; a' b'];
-
-        fmt = [repmat('%10.5f ', 1, length(samples)-1), '%10.5f\n'];
-        fprintf(fid, fmt, samples);
-    end
     plot(P1(1), P1(2), 'ro', ...
         'MarkerEdgeColor','k',...
         'MarkerFaceColor','g',...
@@ -87,8 +92,8 @@ for i=1:1
         'MarkerFaceColor','y',...
         'MarkerSize',5);
 
-    quiver( P1(1), P1(2), LVEC*cos(a1), LVEC*sin(a1), 'Color', 'r' );
-    quiver( P2(1), P2(2), LVEC*cos(a2), LVEC*sin(a2), 'Color', 'r' );
+    quiver( P1(1), P1(2), LVEC*cos(a1), LVEC*sin(a1), 'Color', 'r','LineWidth',1 );
+    quiver( P2(1), P2(2), LVEC*cos(a2), LVEC*sin(a2), 'Color', 'r','LineWidth',1 );
 end
 
 fclose(fid);
