1d66c3daf24a06912c84a362c6cab7cf4bf8e520
Placido <planora@hotmail.com>
2021-12-15 14:16:10 +0100

    Added test script for path generation

---
+++ MATLAB_path_generation/test_path_generation.m
@@ -0,0 +1,21 @@
+close all;
+clear classes;
+clc;
+
+% Folder tree
+addpath(genpath('./synthetic_path_generators/'));
+addpath(genpath('./Clothoids/'));
+
+% Parameters
+num_traj   = 10;
+num_points = 50;
+generator = 'clothoids_PRM_montecarlo_map';
+map = 'cross'; % 'void', 'cross'
+
+options.save = false;
+options.plot = true;
+
+% Generate data
+
+% Call path clustering
+samples = call_generators(generator, map, num_traj, num_points, options);
