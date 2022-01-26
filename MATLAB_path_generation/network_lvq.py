81c199539d4230a3f631b97a2f26065ace590ba8
Alessandro <alessandro.antonucci@unitn.it>
2022-01-26 18:57:13 +0100

    Added LVQ network

---
+++ MATLAB_path_generation/network_lvq.py
@@ -0,0 +1,104 @@
+import sys
+sys.path.append("./libraries/Modified-SOM")
+
+import numpy as np
+import matplotlib.pyplot as plt
+from detection.competitive_learning import CombineSomLvq
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+class Network:
+    def __init__(self):
+        self.model = []
+        self.batch_size = []
+        # Label encoder
+        self.encoder = LabelEncoder()
+
+    def define_model(self, map_size=(10,10)):
+        map_size = np.asarray(map_size).astype(int)
+        # Setting the random state
+        random_state = 17
+        self.model = CombineSomLvq(n_rows=map_size[0], n_cols=map_size[1], random_state=random_state)
+        return self.model
+
+    def prepare_data(self, X, y, training_percentage=70, batch=32, randomize=True):
+        X = np.asarray(X)
+        y = np.asarray(y)
+        self.batch_size = int(batch)
+
+        if randomize:
+            indices = np.arange(X.shape[0])
+            np.random.shuffle(indices)
+            X = X[indices]
+            y = y[indices]
+
+        num_of_samples = X.shape[0]
+        train = int(training_percentage*num_of_samples/100)
+        valid = num_of_samples-train
+
+        if train < self.batch_size or valid < self.batch_size:
+            self.batch_size = 1
+        else:
+            # Samples must be multiplier of batch
+            train = int(train/self.batch_size) * self.batch_size
+            valid = num_of_samples-train
+            valid = int(valid/self.batch_size) * self.batch_size
+
+        X_train = X[0:train, :]
+        y_train = y[0:train]
+        X_valid = X[train:train+valid, :]
+        y_valid = y[train:train+valid]
+
+        y_train = self.encoder.fit_transform(y_train)
+
+        self.X_train = np.array(X_train)
+        self.y_train = np.array(y_train)
+        self.X_valid = np.array(X_valid)
+        self.y_valid = np.array(y_valid)
+
+        return self.X_train, self.y_train, self.X_valid, self.y_valid
+
+    def train_model(self, X_train, y_train, epochs=50, learn_rate=0.01):
+        X_train = np.asarray(X_train)
+        y_train = np.asarray(y_train).astype(np.int8)
+        epochs = int(epochs)
+
+        # Train the LVQ
+        self.model.fit(X_train, y_train, weights_init = "pca", labels_init = None,
+            unsup_num_iters = epochs, unsup_batch_size = self.batch_size,
+            sup_num_iters = epochs, sup_batch_size = self.batch_size,
+            neighborhood = "gaussian",
+            learning_rate = learn_rate, learning_decay_rate = 1, learning_rate_decay_function = None,
+            sigma = 1, sigma_decay_rate = 1, sigma_decay_function = None,
+            conscience = False, verbose = 1)
+        return self.model
+
+    def predict(self, X_valid, y_valid):
+        X_valid = np.asarray(X_valid)
+        y_valid = np.asarray(y_valid)
+        # Predict the result
+        y_pred = self.model.predict(X_valid)
+        y_pred = self.encoder.inverse_transform(y_pred)
+        # Make confusion matrix
+        cm = confusion_matrix(y_valid, y_pred)
+        # Print the confusion matrix
+        print(cm)
+        print('Accuracy:', accuracy_score(y_valid, y_pred))
+        y_pred = np.array(y_pred)
+        return y_pred
+
+    def plot(self):
+        # Plot
+        som_weights = self.model.get_layer(name='SOM').get_weights()[0]
+        
+        fig1, axes = plt.subplots(nrows=self.map_size[0], ncols=self.map_size[1], figsize=(10, 10))
+        for k in range(self.map_size[0] * self.map_size[1]):
+           axes[k // self.map_size[1]][k % self.map_size[1]].imshow(som_weights[k].reshape(2, self.units), cmap='gray')
+           axes[k // self.map_size[1]][k % self.map_size[1]].axis('off')
+        plt.subplots_adjust(hspace=0.05, wspace=0.05)
+        
+        plt.draw() # non-blocking plot
+        plt.pause(0.1)
+
+#=======================================================================================#
--- MATLAB_path_generation/trajectories_features.m
+++ MATLAB_path_generation/trajectories_features.m
@@ -3,10 +3,14 @@ clear classes;
 clc;
 
 % Parameters
-num_traj   = 10;                         % number of trajectories
-num_points = 100;                        % MINIMUM number of points
+num_traj    = 50;                        % number of trajectories
+num_points  = 150;                       % MINIMUM number of points
+num_classes = 3;                         % number of classes
 generator = 'clothoids_PRM_montecarlo';  % path planner
-map = 'test';                            % map: 'void', 'cross', 'povo', 'test', 'thor1'
+map = 'void';                            % map: 'void', 'cross', 'povo', 'test', 'thor1'
+window = 10;
+
+%dataset = 'edinburgh_10Sep';
 
 options.save = false;
 options.plot = true;
@@ -14,21 +18,51 @@ options.plot = true;
 %-------------------------------------------------------------------------%
 
 % Folder tree
+addpath(genpath('./libraries/'));
 addpath(genpath('./functions/'));
 addpath(genpath('./synthetic_path_generators/'));
-addpath(genpath('./Clothoids/'));
-addpath(genpath('./C2xyz_v2/'));
+addpath(genpath('./datasets/'));
 
 colors = customColors;
 
 % Import python module
-pymodule = py.importlib.import_module('network_som');
+pymodule = py.importlib.import_module('network_lvq');
 py.importlib.reload(pymodule);
 
 %% Generate data
 
-% Call path generator
-myTrajectories = call_generators(generator, map, num_traj, num_points, options);
+% Load dataset
+%myTrajectories = load_dataset(dataset, num_points, options);
+
+load('data.mat');
+
+%{
+samples_x = [];
+samples_y = [];
+samples_theta = [];
+encoding = [];
+
+for i = 1:num_classes
+    % Call path generator
+    myTrajectories = call_generators(generator, map, num_traj, num_points, options);
+    
+    %figure(101);
+    %hold on, grid on, box on, axis equal;
+    %xlabel('x (m)');
+    %xlabel('y (m)');
+    %title('Dataset');
+    %cellfun(@plot, myTrajectories.x, myTrajectories.y);
+    
+    % Extract samples
+    if min(cellfun(@length, myTrajectories.x)) < num_points
+        error("Not enough points for the trajectories.");
+    end
+    samples_x = [samples_x; cell2mat(cellfun(@(X) X(1:num_points), myTrajectories.x, 'UniformOutput', false)')];
+    samples_y = [samples_y; cell2mat(cellfun(@(X) X(1:num_points), myTrajectories.y, 'UniformOutput', false)')];
+    samples_theta = [samples_theta; cell2mat(cellfun(@(X) X(1:num_points), myTrajectories.theta, 'UniformOutput', false)')];
+    
+    encoding = [encoding; ones(num_traj,1)*(i-1)];
+end
 
 % Plot dataset
 figure(1);
@@ -36,53 +70,67 @@ hold on, grid on, box on, axis equal;
 xlabel('x (m)');
 xlabel('y (m)');
 title('Dataset');
-cellfun(@plot, myTrajectories.x, myTrajectories.y);
-%for i =1:num_traj
-%    plot(squeeze(samples(i,1,:)), squeeze(samples(i,2,:)));
-%end
+plot(samples_x', samples_y');
+
+% Prepare data
+X = [];
+y = [];
+k = 1;
+for i = 1:size(samples_x,1)
+    for j = 1:num_points-(window-1)
+         X(k,:) = [samples_x(i,j:j+(window-1)), samples_y(i,j:j+(window-1)), samples_theta(i,j:j+(window-1))];
+         y(k,1) = encoding(i);
+         k = k+1;
+    end
+end
 
 % Shift trajectories to origin
 %shift_samples = samples - samples(:,:,1);
-
 % Normalise samples
 %norm_samples = (samples-min(samples,[],3))./(max(samples,[],3)-min(samples,[],3));
-
 % Denormalise samples
 % denorm_samples = norm_samples.*(max(samples,[],3)-min(samples,[],3)) + min(samples,[],3);
-
-% Extract samples
-if min(cellfun(@length, myTrajectories.s)) < num_points
-    error("Not enough points for the trajectories.");
-end
-samples(:,1,:) = cell2mat(cellfun(@(X) X(1:num_points), myTrajectories.x, 'UniformOutput', false)');
-samples(:,2,:) = cell2mat(cellfun(@(X) X(1:num_points), myTrajectories.y, 'UniformOutput', false)');
+%}
 
 %% Train network
 
-epochs = 1000;
+epochs = 200;
 batch  = 128;
 learn_rate = 0.005;
 som_size = [10, 10];
 
 % Initialize network and load Keras model
 %pynet = pymodule.Network(epochs, batch, learn_rate);
-pynet = pymodule.Network(som_size, epochs, batch, learn_rate);
+%pynet = pymodule.Network(som_size, epochs, batch, learn_rate);
+pynet = pymodule.Network();
 
 % Define neural network model
-pynet.define_model(num_points);
+pynet.define_model(som_size);
 
 % encoder = models{1}; encoder.summary();
 % decoder = models{2}; decoder.summary();
 % autoencoder = models{3};
 
 % Load dataset
-data = pynet.prepare_data(samples, 80);
+data = pynet.prepare_data(X, y, 80, batch, 1);
 
 X_train = double(data{1});
-X_valid = double(data{2});
+y_train = double(data{2});
+X_valid = double(data{3});
+y_valid = double(data{4});
+
+%%
 
 % Train network
-model = pynet.train_model(X_train);
+model = pynet.train_model(X_train, y_train, epochs, learn_rate);
+
+y_pred = pynet.predict(X_valid, y_valid);
+y_pred = double(y_pred);
+
+%% SOM
+
+%{
+som = SOMSimple(10, epochs, samples, 0.1, 0.05, 20, 0.05, 2);
 
 % Plot
 som_weights = double(model.get_layer('SOM').get_weights{1});
@@ -96,6 +144,58 @@ for k = 1:som_size(1)*som_size(2)
     %set(gca,'visible','off');
 end
 
+%-------------------------------------------------------------------------%
+
+% Create a Self-Organizing Map
+net = selforgmap([som_size], 'topologyFcn', 'gridtop');
+net.trainParam.epochs = epochs;
+
+aaa = reshape(X_train, 164, 200);
+bbb = reshape(X_valid, 41, 200);
+
+% Train the Network
+[net,tr] = train(net,aaa);
+
+% Test the Network
+y = net(bbb);
+
+% View the Network
+view(net)
+
+op_som = vec2ind(net(aaa))';
+
+op_som_7 = find(op_som == 7);
+aaa_7 = aaa(op_som_7)
+
+% Plots
+% Uncomment these lines to enable various plots.
+%figure, plotsomtop(net)
+%figure, plotsomnc(net)
+%figure, plotsomnd(net)
+%figure, plotsomplanes(net)
+figure, plotsomhits(net,aaa(1,:,:))
+figure, plotsompos(net,aaa(1,:,:))
+
+%-------------------------------------------------------------------------%
+
+angles = 0:0.5*pi/99:0.5*pi;
+X = [sin(angles); cos(angles)];
+plot(X(1,:),X(2,:),'+r')
+
+net = selforgmap(10);
+
+net.trainParam.epochs = 10;
+net = train(net,X);
+
+plotsompos(net)
+
+x = [0.4;0.9];
+a = net(x)
+
+vec2ind(a)'
+
+%-------------------------------------------------------------------------%
+%}
 
 %%
 
@@ -165,4 +265,3 @@ xlabel('samples');
 legend({'x','y'});
 title('Rmse');
 %}
-
