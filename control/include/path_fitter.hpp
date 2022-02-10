975a7134c7f221c204f8fdebca72a244a9a9209a
Alessandro <alessandro.antonucci@unitn.it>
2022-02-10 15:51:46 +0100

    Attempt to make it work

---
+++ control/include/path_fitter.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+
+struct Point { 
+  int t;
+  double x, y;
+
+  Point(int t, double x, double y): t(t), x(x), y(y) {}
+  Point(): Point(0, 0., 0.) {}
+};
+
+int t0 = 0;
+std::vector<Point> pts;
+std::vector<Point> fit;
+std::vector<int>   st; // max t up to the beginning of the i-th segment
